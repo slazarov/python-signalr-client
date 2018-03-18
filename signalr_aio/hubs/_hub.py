33f58244b15ab6056cb0a0ad4ad53b040aacb8e8
slazarov <s.a.lazarov@gmail.com>
2018-03-18 10:57:23 +0200

    Omit redundant dict in message.

--- signalr_aio/hubs/_hub.py
+++ signalr_aio/hubs/_hub.py
@@ -40,7 +40,7 @@ class HubClient(object):
                 if hub.lower() == self.name.lower():
                     method = inner_data['M']
                     message = inner_data['A']
-                    await self.__handlers[method](message)
+                    await self.__handlers[method](inner_data)
 
         connection.received += handle
 
